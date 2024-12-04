eaddda34469c95da0d796c24a8f2c3a0f7ef96b2
RomanPonce <romanponce202@gmail.com>
2024-12-04 18:34:18 -0300

    todo sirve señores

--- app.js
+++ app.js
@@ -5,60 +5,67 @@ const tablero = document.querySelector(".gamerboard");
 // condigo consol(sirve)
 let ganador = false;
 // Ayuda
+// i = 0
 // 0 1 2
 // 3 4 5
 // 6 7 8
 
-const LineaTop = [1,1,1,0,0,0,0,0,0];
-const LineaMid = [0,0,0,1,1,1,0,0,0];
-const LineaMBotton= [0,0,0,0,0,0,1,1,1];
-const noGana= [0,1,1,1,0,0,0,0,0];
+const columnaUno = [1,0,0,1,0,0,1,0,0];
+const columnaDos = [0,1,0,0,1,0,0,1,0];
+const columnaTres = [0,0,1,0,0,1,0,0,1];
 
+const diagonalC = [0,0,1,0,1,0,1,0,0];
+const diagonalD = [1,0,0,0,1,0,0,0,1];
 
-function buscarGanador(arr, marca){
+const noGana = [0, 1, 1, 1, 0, 0, 0, 0, 0];
+
+function buscarGanador(arr, marca) {
   const ganaPor = {
-    linea: 0,
-    columna: 0,
-    diagonalDecreciente: 0,
-    diagonalCreciente: 0
-  }
-  
+    linea: false,
+    columna: false,
+    diagonalDecreciente: false,
+    diagonalCreciente: false,
+  };
+  // Linea
   for (let i = 0; i <= arr.length; i += 3) {
-    if (arr[i] === marca && arr[i + 1] === marca && arr[i + 2] === marca){
-      ganaPor.linea++;
+    if (arr[i] === marca && arr[i + 1] === marca && arr[i + 2] === marca) {
+      ganaPor.linea = true;
     }
   }
 
-  for (let i = 0; i <= arr.length; i += 3) {
-    if (arr[i] === marca && arr[i + 3] === marca) {
-      ganaPor.columna++;
+  // Columna
+  for (let i = 0; i <= 3; i++) {
+    if (arr[i] === marca && arr[i + 3] === marca && arr[i + 6] === marca) {
+      ganaPor.columna = true;
     }
+  }
+  // Diagonal 1
+  if (arr[0] === marca && arr[4] === marca && arr[8] === marca) {
+    ganaPor.diagonalDecreciente = true;
   }
 
-  for (let i = 0; i <= arr.length; i += 4) {
-    if (arr[i] === marca && arr[i + 4] === marca) {
-      ganaPor.diagonalDecreciente++;
-    }
+  // Diagonal 2
+  if (arr[2] === marca && arr[4] === marca && arr[6] === marca) {
+    ganaPor.diagonalCreciente = true;
   }
 
-  for (let i = 0; i <= arr.length; i += 2) {
-    if (arr[i] === marca && arr[i + 2] === marca) {
-      ganaPor.diagonalCreciente++;
-    }
-  }
-  
   return ganaPor;
 }
-const ganaLT = buscarGanador(LineaTop,1);
-const ganaLM = buscarGanador(LineaMid,1);
-const ganaLB = buscarGanador(LineaMBotton,1);
-const nowin = buscarGanador(noGana,1);
+const ganaCF = buscarGanador(columnaUno, 1);
+const ganaCS = buscarGanador(columnaDos, 1);
+const ganaCT = buscarGanador(columnaTres, 1);
 
-console.log("Gana en linea alta todo el objeto:");
-console.log(ganaLT);
-console.log("Gana en linea media todo el objeto:");
-console.log(ganaLM);
-console.log("Gana en liena baja todo el objeto:");
-console.log(ganaLB);
-console.log("No deberia ganar");
-console.log(nowin);
+const ganaDC = buscarGanador(diagonalC, 1);
+const ganaDD = buscarGanador(diagonalD, 1);
+
+
+console.log("Gana en columna 1 todo el objeto:");
+console.log(ganaCF);
+console.log("Gana en columna 2 todo el objeto:");
+console.log(ganaCS);
+console.log("Gana en columna 3 todo el objeto:");
+console.log(ganaCT);
+console.log("Gana en diagonal creciente todo el objeto:");
+console.log(ganaDC);
+console.log("Gana en diagonal decreciente todo el objeto:");
+console.log(ganaDD);
